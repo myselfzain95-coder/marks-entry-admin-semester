1b273277321536879a51a77005b59373580b5fb0
Muhammad Zain <myselfzain95@gmail.com>
2026-09-16 01:51:06 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
-   import sys, os
-   sys.path.append(os.path.join(os.path.dirname(__file__), "..", "backend"))
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "backend"))
 
-   from main import app
+from fastapi import FastAPI
+from main import app as backend_app
+
+app = FastAPI()
+app.mount("/api", backend_app)
 
 
 
